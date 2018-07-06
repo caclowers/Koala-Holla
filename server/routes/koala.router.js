95f92b4cd6bfa1b7589690485c2cb6df57b6aacd
Casey <caclowers@gmail.com>
2018-07-06 11:29:37 -0500

    created /koala router.post function

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -4,16 +4,29 @@ const Koala = require('../models/koala.schema.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('got to koala GET');
+    console.log('line 7 got to koala GET');
     Koala.find({}).then((data) => {
-        console.log('here is out data', data);
+        console.log('line 9 here is out data', data);
         res.send(data);
     }).catch((err) => {
-        console.log('catch error', err);
+        console.log('line 12 catch error', err);
         res.sendStatus(500);  
-    })
+    });
+});
+
+router.post('/', (req, res) => {
+    console.log('line 18 got to /koala POST');
+    console.log('line 19 koala POST req.body is:', req.body);
     
-    //res.sendStatus(200);
+    let newKoala = new Koala(req.body);
+
+    newKoala.save().then((data) => {
+        console.log('POST data from Mongoose:', data);
+        res.sendStatus(201);
+    }).catch((err) => {
+        console.log('koala POST failed. Error:', err);
+        res.sendStatus(500);
+    });
 });
 
 
@@ -22,7 +35,7 @@ router.get('/', (req, res) => {
 
 
 
-router.post('/',);
+
 
 router.put('/:id',);
 
--- server/server.js
+++ server/server.js
@@ -31,14 +31,6 @@ app.post('/koala', (req, res) => {
   console.log('POST to /koala req.body =', req.body);
 });
 
-// GET
-app.get('/koala', (req, res) => {
-  // Temporary mock data. Replace this with mongoose.
-  const mockData = [{ _id: 1, name: 'Mock 1', gender: 'F', age: 5, ready_to_transfer: true, notes: 'n/a' },
-                    { _id: 2, name: 'Mock 2', gender: 'M', age: 5, ready_to_transfer: false, notes: 'n/a' }]
-  res.send(mockData);
-})
-
 // Static files
 app.use(express.static('server/public'));
 
