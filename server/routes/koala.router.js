f4bc7eba2f063cbb8203138fb6def9982a939826
Casey <caclowers@gmail.com>
2018-07-06 11:44:45 -0500

    created /koala router.delete function

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
     });
 });
 
+
 router.post('/', (req, res) => {
     console.log('line 18 got to /koala POST');
     console.log('line 19 koala POST req.body is:', req.body);
@@ -21,25 +22,29 @@ router.post('/', (req, res) => {
     let newKoala = new Koala(req.body);
 
     newKoala.save().then((data) => {
-        console.log('POST data from Mongoose:', data);
+        console.log('line 24 POST data from Mongoose:', data);
         res.sendStatus(201);
     }).catch((err) => {
-        console.log('koala POST failed. Error:', err);
+        console.log('line 27 koala POST failed. Error:', err);
         res.sendStatus(500);
     });
 });
 
 
-
-
-
-
-
-
-
 router.put('/:id',);
 
-router.delete('/:id',);
+router.delete('/:id', (req, res) => {
+    console.log('line 37 here is the req.body from frontend:', req.body);
+    Koala.findByIdAndRemove({
+        _id : req.params.id;
+    }).then((data) => {
+        console.log('line 41 data returned from Mongo:', data);
+        res.sendStatus(202);
+    }).catch((err) => {
+        console.log('line 44 koala delete request failed. Error:', err);
+        res.sendStatus(500);   
+    });
+});
 
 
 module.exports = router;
