31db9a0e7e72f211f854da5aea8976656b0bc811
Casey <caclowers@gmail.com>
2018-07-06 11:03:26 -0500

    server is running and connected to mongoose

--- server/server.js
+++ server/server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+const databaseUrl = `mongodb://localhost:27017/koala`;
+mongoose.connect(databaseUrl);
+
+mongoose.connection.on('connected', () => {
+  console.log(`mongoose connected to ${databaseUrl}`);
+  
+});
+
+mongoose.connection.on('error', (error) => {
+  console.log(`mongoose connection error ${error}`);
+  
+});
 
 // Configure body-parser for Angular and jQuery
 app.use(bodyParser.urlencoded({ extended: true }));
