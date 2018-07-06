99f8d2a4efe428194a77265a1b8dbd6d04d0f7ce
Jimmy Brannon <user.email>
2018-07-06 10:47:47 -0500

    add inputs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,9 @@
-console.log( 'js' );
+const app = angular.module('KoalaApp', []);
+
+app.controller('KoalaController', ['$http', function($http){
+  const self = this;
+
+  self.addKoala = function(newKoala){
+    console.log(newKoala);
+  }
+}])
